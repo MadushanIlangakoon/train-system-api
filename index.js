ceffba7672f0e2da73d0c5ff1892d07fe4c6dd49
Ilangakoon Mudiyanselage Kavishka Madushan <146872058+MadushanIlangakoon@users.noreply.github.com>
2024-06-17 23:39:52 +0530

    Testing Merge

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const port = 80;
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
-app.get('/hi', (req, res) => {
-    res.send('Hello, world2!');
-  });
+
 // Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
