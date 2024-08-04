1eb6837705fe729cacd55cc49f9f00991dff708a
Ilangakoon Mudiyanselage Kavishka Madushan <146872058+MadushanIlangakoon@users.noreply.github.com>
2024-08-05 01:16:41 +0530

    adding seq token

--- app.js
+++ app.js
@@ -10,6 +10,35 @@ const cors = require('cors'); // Import cors
 const app = express();
 
 connectDB();
+
+
+const verifyApiKey = (req, res, next) => {
+  // const token = req.headers['authorization']?.split(' ')[1];
+  const token = req.headers['authorization'];
+  const publicKey = process.env.PRIV_KEY;
+
+  if (!token) {
+    return res.status(403).json({ error: 'Forbidden: No token provided' });
+  }
+
+  // try {
+  //   const decoded = jwt.verify(token, publicKey, { algorithms: ['RSA256'] });
+  //   req.user = decoded;
+  //   next();
+  // } catch (error) {
+  //   return res.status(403).json({ error: 'Forbidden: Invalid token' });
+  // }
+  try {
+    if (token !== process.env.API_SIMPLE_KEY) {
+      return res.status(403).json({ error: 'Forbidden: Invalid API key' });
+    }
+    next();
+  } catch (error) {
+    return res.status(403).json({ error: 'Forbidden: Invalid token' });
+  }
+  
+};
+
 // app.use(cors({ origin: 'http://localhost:3000' })); // Enable CORS for requests from http://localhost:3000
 app.use(cors({ origin: true }));
 app.use(bodyParser.json());
@@ -20,7 +49,7 @@ app.get('/', (req, res) => {
 });  
 
 // Use routes
-app.use('/api', trainRoutes);
+app.use('/api', verifyApiKey, trainRoutes);
 
 // Export the app
 module.exports = app;
